68d2cd2e1ff5387dd6a49a40f35994ae3626722d
Hassooo1 <asnanhafta@gmail.com>
2025-05-30 11:35:05 +0500

    Adding preprocessing

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('hello word')
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+
+import re
+import nltk
+
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt', quiet=True)
+nltk.download('wordnet', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+
+
+
+def preprocess_text(text):
+    # Make text lowercase and remove links, text in square brackets, punctuation, and words containing numbers
+    text = str(text)
+    text = text.lower()
+    text = re.sub(r'https?://\S+|www\.\S+|\[.*?\]|[^a-zA-Z\s]+|\w*\d\w*', ' ', text)
+    text = re.sub(r'\n', ' ', text)
+
+    # Remove stop words
+    stop_words = set(stopwords.words("english"))
+    stop_words.remove('not')
+    words = text.split()
+    filtered_words = [word for word in words if word not in stop_words]
+    text = ' '.join(filtered_words).strip()
+
+    # Tokenize
+    tokens = nltk.word_tokenize(text)
+
+    # Lemmatize
+    lemmatizer = WordNetLemmatizer()
+    porter = PorterStemmer()
+    lem_tokens = [porter.stem(lemmatizer.lemmatize(token)) for token in tokens]
+    
+    return ' '.join(lem_tokens)
