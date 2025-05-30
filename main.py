1b9bc18945378ddbaea978d8f99eaea97d6080bc
Hassooo1 <asnanhafta@gmail.com>
2025-05-30 11:47:19 +0500

    Adding display_result function and perform single reviews

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 
-
+import streamlit as st
 import re
 import nltk
+import pickle
 
 nltk.download('stopwords', quiet=True)
 nltk.download('punkt', quiet=True)
@@ -36,3 +37,91 @@ def preprocess_text(text):
     lem_tokens = [porter.stem(lemmatizer.lemmatize(token)) for token in tokens]
     
     return ' '.join(lem_tokens)
+
+def display_result(result):
+    if result[0]=="Positive":
+        st.subheader(result[0]+":smile:")
+    elif result[0]=="Negative":
+        st.subheader(result[0]+":pensive:")
+    else:
+        st.subheader(result[0]+":neutral_face:")
+
+
+
+if __name__ == "__main":
+    # ========================
+    # App Configuration
+    # ========================
+    st.set_page_config(page_title="Sentimatic: Streamlining Customer Feedback Analysis", page_icon="💬", layout="centered")
+
+    # ========================
+    # Custom Styles
+    # ========================
+    st.markdown("""
+        <style>
+            .stTextInput>div>div>input {
+                border-radius: 8px;
+                padding: 10px;
+            }
+            .stButton>button {
+                background-color: #4B0082;
+                color: white;
+                border-radius: 8px;
+                font-weight: bold;
+                padding: 8px 16px;
+            }
+            .main {
+                background-color: #f6f5f6;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    # ========================
+    # Layout - Title & Description
+    # ========================
+    
+    st.title("💬 Sentimatic")
+    st.subheader("Streamlining Customer Feedback Analysis with Sentiment Intelligence")
+    st.divider
+    classifier = st.selectbox(
+        "Which classifier do you want to use?",
+        ["Logistic Regression", "Random Forest", "Gradient Boosting Classifier", "Naive Bayes", "Support Vector Machine (SVM)"])
+    if classifier == 'Logistic Regression':
+        st.write('You selected Logistic Regression')
+    elif classifier == 'Naive Bayes':
+        st.write('You selected Naive Bayes')
+    elif classifier == 'Random Forest':
+        st.write('You selected Random Forest')
+    elif classifier == 'Gradient Boosting Classifier':
+        st.write('You selected Gradient Boosting Classifier')
+    else:
+        st.write("You selected SVM")
+    st.divider()
+    
+    with open("S1_model.p", 'rb') as mod:
+            data = pickle.load(mod)
+    vect = data['vectorizer']
+
+    if classifier=="Logistic Regression":
+        model = data["lg"]
+    elif classifier=="Naive Bayes":
+        model = data["nb"]
+    elif classifier == 'Random Forest':
+        model = data["rf"]
+    elif classifier == 'Gradient Boosting Classifier':
+        model = data["gbm"]
+    else:
+        model = data["svm"]
+
+
+    st.subheader('Check sentiments of a single review:')
+    single_review = st.text_area("Enter review:")
+    if st.button('Check the sentiment!'):
+        review = preprocess_text(single_review)
+        inp_test = vect.transform([single_review])
+        result = model.predict(inp_test)
+        print(result)
+        display_result(result)
+        
+    else:
+        st.write('')
